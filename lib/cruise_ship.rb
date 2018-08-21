6485c273967099e02fd19476a39e610227c1ddbf
braj011 <bhagyaraj01@gmail.com>
2018-08-21 23:03:07 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,6 +11,7 @@ def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name  
+      # empty string is overwritten with winning name
     end 
   end 
   winner
